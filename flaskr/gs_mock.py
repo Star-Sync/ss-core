8f3f42dcb6df063481ae2f7cdea8d13d721675fa
Felo Tadros <felotadros326@gmail.com>
2024-11-07 13:26:27 -0500

    feat: create gs_mock API endpoint

--- flaskr/gs_mock.py
+++ flaskr/gs_mock.py
@@ -1,27 +1,55 @@
-import datetime
-from flask import jsonify
+from datetime import datetime
+from flask import request, jsonify
 import numpy as np
+from pydantic import BaseModel
+from typing import List
 
-from dataclasses import dataclass
 
-
-@dataclass
-class mockRequest:
+class MockRequest(BaseModel):
     start: datetime
     end: datetime
-    delta: int
+    delta_minutes: int
 
 
-def generate_mock_data(request: mockRequest):
+def gs_mock():
+    print("gs_mock")
+    if request.method != "POST":
+        return jsonify({"error": "Method not allowed"}), 405
+    if not request.json or not all(
+        key in request.json for key in ["start", "end", "delta"]
+    ):
+        return jsonify({"error": "Missing JSON in request"}), 400
+
+    start = request.json.get("start")
+    end = request.json.get("end")
+    delta = request.json.get("delta")
+
+    start = datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
+    end = datetime.strptime(end, "%Y-%m-%d %H:%M:%S")
+    delta = int(delta)
+
+    mr = MockRequest(start=start, end=end, delta_minutes=delta)
+
+    random_array = generate_mock_data(mr)
+    # so, jsonify wil return a json object right
+    # and itll will jsonify the python boolean values
+    # meaning itll return JS boolean values
+    # (true and false, not "True" and "False", nor 1 and 0)
+    return jsonify(random_array)
+
+
+def generate_mock_data(request: MockRequest) -> List[bool]:
     start = request.start
     end = request.end
-    delta = request.delta
+    delta = request.delta_minutes
+
+    start = start.timestamp()
+    end = end.timestamp()
+
+    if start is None or end is None or delta is None:
+        raise ValueError("Missing parameters")
 
     # find the number of delta intervals between start and end
-    if start is None or end is None or delta is None:
-        return jsonify({"error": "Missing parameters"}), 400
-
     size = int((end - start) / delta)
     random_array = np.random.choice([True, False], size=size).tolist()
-
-    return jsonify(random_array)
+    return random_array
