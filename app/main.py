30e80a0692ba2bd642f5fa0621145be39d5eeb9d
Felo Tadros <felotadros326@gmail.com>
2024-11-08 16:57:57 -0500

    refactor to match models, services, routers

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
-from .routers import gs_mock, hello
+from .routers import gs, hello
 
 app = FastAPI()
 
-app.include_router(gs_mock.router, prefix="/api/v1")
+app.include_router(gs.router, prefix="/api/v1")
 app.include_router(hello.router, prefix="/api/v1")
 
 
