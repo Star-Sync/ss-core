07b83de6ae5f1defd544023ab9339d22d41edc66
Felo Tadros <felotadros326@gmail.com>
2024-11-21 18:38:35 -0500

    feat: implement database session management and add Mission model

--- app/routers/hello.py
+++ app/routers/hello.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 router = APIRouter(
     prefix="/hello",
@@ -6,6 +7,28 @@ router = APIRouter(
 )
 
 
+class Mission(SQLModel, table=True):
+    missionid: int | None = Field(default=None, primary_key=True)
+    missionname: str = Field(sa_column_kwargs={"nullable": False, "unique": True})
+
+
 @router.get("/")
 def hello():
+    # use ..services.db to get a database session
+    # and just return a simple message
+    from ..services.db import get_db
+
+    # read the tables
+    db = next(get_db())
+
+    # create a mission
+    mission = Mission(missionname="Mission 1")
+    db.add(mission)
+    db.commit()
+    db.refresh(mission)
+    statement = select(Mission)
+    results = db.exec(statement)
+    for mission in results:
+        print(mission)
+
     return {"message": "Hello, World!"}
---
+++ app/services/db.py
@@ -0,0 +1,18 @@
+from sqlmodel import Session, create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = "postgresql://starsync:starsync@ss-db:5432/starsync"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+def get_db():
+    """
+    Dependency to provide a database session.
+    Ensures the session is properly closed after use.
+    """
+    with Session(engine) as session:
+        yield session
