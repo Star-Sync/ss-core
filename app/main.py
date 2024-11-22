21088eb520a7b3bbe28ba6a16cf771a6bff48f2d
Felo Tadros <felotadros326@gmail.com>
2024-11-22 16:14:19 -0500

    fix: added CORS middleware and updated router to include mission

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from .routers import gs, hello, request
+from .routers import gs, hello, mission
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(gs.router, prefix="/api/v1")
 app.include_router(hello.router, prefix="/api/v1")
-app.include_router(request.router, prefix="/api/v1")
+app.include_router(mission.router, prefix="/api/v1")
 
 
 @app.get("/")
