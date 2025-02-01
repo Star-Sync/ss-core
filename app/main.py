fdcb3728881d9600b7f4ac4624b23a9f3bcdc6a1
Felo Tadros <felotadros326@gmail.com>
2025-02-01 13:34:05 -0500

    add logging configuration and test GELF integration in FastAPI

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pygelf import GelfTcpHandler, GelfUdpHandler, GelfTlsHandler, GelfHttpHandler, GelfHttpsHandler
+import logging
+import http.client
+import json
 
 from .routers import gs, hello, request
 
@@ -18,6 +22,31 @@ app.include_router(hello.router, prefix="/api/v1")
 app.include_router(request.router, prefix="/api/v1")
 
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+logger.addHandler(GelfUdpHandler(host='localhost', port=12201))
+
+def test():
+    conn = http.client.HTTPConnection("localhost", 12201)
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    data = {
+        "version": "1.1",
+        "host": "example.org",
+        "short_message": "A short message",
+        "level": 5,
+        "_some_info": "foo"
+    }
+
+    json_data = json.dumps(data)
+    conn.request("POST", "/gelf", body=json_data, headers=headers)
+    response = conn.getresponse()
+
+    print(response.status)
+    print(response.read().decode())
+
 @app.get("/")
 async def root():
+    test()
     return {"message": "Hello FastAPI!"}
