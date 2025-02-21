f985d8d569e2f52837e7da8c84bea8bb37771bbc
Felo Tadros <felotadros326@gmail.com>
2025-02-21 12:44:57 -0500

    Fix logging configuration and add console output

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.docs import get_swagger_ui_html
-from fastapi.responses import JSONResponse
-
+from pygelf import GelfTcpHandler
 from .routers import gs, hello, request, ground_station
+import logging
+
+logger = logging.getLogger()
 
 app = FastAPI()
 
@@ -23,4 +25,5 @@ app.include_router(ground_station.router, prefix="/api/v1")
 
 @app.get("/", include_in_schema=False)
 async def root():
+    logger.info("Root endpoint")
     return get_swagger_ui_html(openapi_url="/openapi.json", title="Star Sync API")
